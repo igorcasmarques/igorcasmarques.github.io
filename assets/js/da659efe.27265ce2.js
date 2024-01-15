9d018c842d974b8d97e1f793061e980266cb0e8c
Igor Marques <coelhoigor@gmail.com>
2024-01-14 21:24:18 -0500

    Add 2 portfolio samples

--- assets/js/da659efe.27265ce2.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkportfolio=self.webpackChunkportfolio||[]).push([[829],{7769:e=>{e.exports=JSON.parse('{"title":"Resume","description":"Select a topic below to learn more about me.","slug":"/category/resume","permalink":"/docs/category/resume","navigation":{"next":{"title":"Education","permalink":"/docs/education"}}}')}}]);
