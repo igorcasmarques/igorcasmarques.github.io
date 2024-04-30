6cfb6e033018aeefcc88dce58504271d7641d03f
Igor Coelho A. S. Marques <coelhoigor@gmail.com>
2024-04-30 17:00:45 -0400

    Add chapter 6 to my story

--- assets/js/12a93631.d4733eda.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkportfolio=self.webpackChunkportfolio||[]).push([[2808],{3537:e=>{e.exports=JSON.parse('{"permalink":"/my-story/page/5","page":5,"postsPerPage":1,"totalPages":5,"totalCount":5,"previousPage":"/my-story/page/4","blogDescription":"This is a (somewhat) brief account of my adult life.","blogTitle":"My story"}')}}]);
