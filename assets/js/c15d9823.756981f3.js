cfe99d20149e65bfaeb0aa5582c1ebc38ecdf9f5
Igor Coelho A. S. Marques <coelhoigor@gmail.com>
2026-04-30 14:27:45 -0400

    New blog post - Slam

--- assets/js/c15d9823.756981f3.js
+++
@@ -1,1 +0,0 @@
-"use strict";(globalThis.webpackChunkportfolio=globalThis.webpackChunkportfolio||[]).push([[8146],{9328(o){o.exports=JSON.parse('{"metadata":{"permalink":"/blog","page":1,"postsPerPage":10,"totalPages":2,"totalCount":12,"nextPage":"/blog/page/2","blogDescription":"Blog","blogTitle":"Blog"}}')}}]);
---
+++ assets/js/c15d9823.d3711ae6.js
@@ -0,0 +1,1 @@
+"use strict";(globalThis.webpackChunkportfolio=globalThis.webpackChunkportfolio||[]).push([[8146],{9328(o){o.exports=JSON.parse('{"metadata":{"permalink":"/blog","page":1,"postsPerPage":10,"totalPages":2,"totalCount":13,"nextPage":"/blog/page/2","blogDescription":"Blog","blogTitle":"Blog"}}')}}]);
